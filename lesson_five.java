8e96eddc64c68b0f9081993376762c4cbc8df385
EricSchles <ericschles@gmail.com>
2014-12-31 08:49:59 -0800

    done with lesson 5

--- lesson_five.java
+++ lesson_five.java
@@ -1,3 +1,28 @@
+/*
+
+How to run:
+
+javac -Xlint lesson_five.java other_class_three.java
+java lesson_five
+
+Explanation:
+
+There is a lot less to explain here.  The biggest point to understand is:
+
+	other_class_three.set_input("Hello there");	
+	other_class_three.print(ref.get_input()); //prints Hello there
+	other_class_three.print(ref_two.get_input()); //prints Hello there
+
+	other_class_three.set_input("Hello, this is different....");	
+	other_class_three.print(ref.get_input()); //prints Hello, this is different....
+	other_class_three.print(ref_two.get_input()); //print Hello, this is different....
+
+Notice the use of static method and how this is reflected in the individual instances of the other_class_three.  
+As we update the variable through the static method, the value is updated in each class.  This shows some more power for instiation.
+
+The only other point worth mentioning here is in the other_class_three.java file.  PLease head there for an explanation of generics.
+
+*/
 class lesson_five{
 	public static void main(String[] args){
 		other_class_three ref = new other_class_three("First thing","Second thing");		
@@ -12,12 +37,12 @@ class lesson_five{
 		other_class_three.print(ref.get_generic());
 
 		other_class_three.set_input("Hello there");	
-		other_class_three.print(ref.get_input());
-		other_class_three.print(ref_two.get_input());
+		other_class_three.print(ref.get_input()); //prints Hello there
+		other_class_three.print(ref_two.get_input()); //prints Hello there
 
 		other_class_three.set_input("Hello, this is different....");	
-		other_class_three.print(ref.get_input());
-		other_class_three.print(ref_two.get_input());
+		other_class_three.print(ref.get_input()); //prints Hello, this is different....
+		other_class_three.print(ref_two.get_input()); //prints Hello, this is different....
 
 	}
 }
--- other_class_three.java
+++ other_class_three.java
@@ -1,3 +1,46 @@
+/*
+
+So Generics are bit tricky.  They basically allow Java to inspect the value of a variable at run time and assign the appropriate type.
+This means your programs don't need to be strongly typed.  
+
+Def Stronly Typed variables := variables with a set type at run time and compile time. 
+
+Example:
+
+String t = "Hello";
+
+t = 7; //throws an error
+
+T t = "Hello"; - where T is a generic type
+t = 7; //passes
+
+That's all there is from an idea prospective, but it makes the syntax really annoying and funky.  But whatever, not my favorite part of
+Java.
+
+So how do you get generics in your code?
+
+Take a look at the class definition:
+class other_class_three<T,T1>
+
+Anything in the <> will be treated as a generic throughout the Java program.  Here we create two, mostly to show you, you can.
+T - is the only one we make use of.
+
+For the most part Generics are used the same way as normal variables except in the following definition:
+
+public static <T> void print(T to_print)
+
+when you create a static method you need to pass in <T> as the generic type.  We could declare a new generic if we wanted to here.
+I'm using T, since it's just easier than having to think of a new name, but I could have named it something else.
+
+As an exercise change the method definition to:
+
+public static <X> void print(X to_print)
+
+and recompile the code (if you don't remember how to do this, please look at lesson_five.java)
+
+The final point is the use of Xlint.  If we didn't use statics and generics we wouldn't need to add this flag.  Explaining why is beyond the scope
+of these lessons, but it might be worth it to look it up yourself :)
+*/
 class other_class_three<T,T1>{
 	
 	public static String input = "initially";
