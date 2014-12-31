cf9cd2209f8487fb6d020ffab672ca9d8abcc22a
EricSchles <ericschles@gmail.com>
2014-12-31 06:35:54 -0800

    added code starting documentation

---
+++ lesson_two.java
@@ -0,0 +1,16 @@
+class lesson_two{
+
+	public static void println(String input){
+		System.out.println(input);
+	}
+
+	public static void print(String input){
+		println(input);
+	}
+	public static void main(String[] args){
+		println("Your first function");
+		println("Isn't that nice?");
+		println("");
+		print("We can even wrap our wrapped function");
+	}
+}
