aa75504d4eea7f4b4153aaedb15587829711bb66
EricSchles <ericschles@gmail.com>
2014-12-31 06:24:07 -0800

    adding how to run, started explanation

--- lesson_one.java
+++ lesson_one.java
@@ -1,3 +1,22 @@
+/*
+How to run:
+
+On Ubuntu:
+
+sudo apt-get install openjdk-7-jdk
+
+javac lesson_one.java
+java lesson_one
+
+Explanation:
+
+The following is a first java program.  It explains all the necessary syntax to get started.  
+
+So what's going on here?  Well we are starting with the typical first example for any programming language - the "hello world" example.
+In this example you are taught how to print to the screen.
+
+
+*/
 class lesson_one {
 	public static void main(String[] args){
 		System.out.println("Welcome to java");
