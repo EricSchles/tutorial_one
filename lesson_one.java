474eb37f8d6d31a12272493792a3727969afd36b
EricSchles <ericschles@gmail.com>
2014-12-31 06:51:10 -0800

    updating java things

--- lesson_one.java
+++ lesson_one.java
@@ -44,6 +44,8 @@ All you need to know about this is anything within the public static void main(S
 Anything not explicitly in this function will not be run, so if you want your code to actually need to be executed it needs to be "inside" this function definition.
 
 So how do you put things inside a function?  With curly braces.  Don't worry if that doesn't make much sense, we'll get into it over the next few lessons.
+
+As a final very important point: the class with the main function must be the same as the file name in Java.  However you can have many classes in a single file.
 */
 class lesson_one {
 	public static void main(String[] args){
--- lesson_two.java
+++ lesson_two.java
@@ -11,7 +11,25 @@ So this code is a bit more complex.  Here we've added our three function definit
 so let's understand what's happening here:
 
 A function in programming is a little different from a function in mathematics.  Unless of course you are talking about functional programming.
-First of all, functions need not return anything.  Secondly, functions need not take in any input
+First of all, functions need not return anything.  Secondly, functions need not take in any input just like PrintSameEveryTime doesn't.
+
+So these functions are all pretty basic, they take in a String named input, and then print that String to the screen.
+
+Notice that each function has curly braces to determine the scope of the function.
+
+Definition: scope := A self contained piece of code that only executes in at a specific time.  
+
+In java scope is specified by curly braces.  So if you want to tell Java what statements to execute inside of a function, you create a 
+function definition and then you simply, put the code you want executed inside the curly braces.  
+
+This concept isn't particularly deep, at least right now, but it's important to understand since its where many of the errors in programming stem from. 
+
+So really in programming, a function is just a way of separating scopes for different pieces of code.  Which is very important.
+
+Now that we understand scope (hopefully), let's go back to our main function.
+
+The way that Java determines what to be executed is by looking at the main function's scope within the class's scope.  
+
 
 */
 class lesson_two{
@@ -27,6 +45,7 @@ class lesson_two{
 	public static void PrintSameEveryTime(){
 		print("This will always print this line, and nothing else");
 	}
+	
 	public static void main(String[] args){
 		println("Your first function");
 		println("Isn't that nice?");
