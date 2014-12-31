c21c48afa4fede586bd70d5ab66c42dd8f64838b
EricSchles <ericschles@gmail.com>
2014-12-31 06:27:52 -0800

    adding to explanation

--- lesson_one.java
+++ lesson_one.java
@@ -15,7 +15,20 @@ The following is a first java program.  It explains all the necessary syntax to 
 So what's going on here?  Well we are starting with the typical first example for any programming language - the "hello world" example.
 In this example you are taught how to print to the screen.
 
+The only 'real' thing happening here is:
 
+System.out.println("Welcome to java");
+
+This explains much of how java works. Every line ends with a semicolon, and function calls require paranthesis.  Also notice the use of '.'
+Specifically in System.out.println.  Theses dots are used to denote class methods.  Specifically we are in the System package of java, in the out class and calling the println method.
+
+We could wrap the println method as well.  (Something will get into in the next lesson).
+
+If all of this is over your head, don't worry!  We'll get into all of it.  
+
+The last thing to explain is the "class lesson_one{" line and the "public static void main(String[] args){" line.
+
+  
 */
 class lesson_one {
 	public static void main(String[] args){
