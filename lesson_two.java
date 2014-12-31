292706b3237004873e87f90037f4d8f6494bcda1
EricSchles <ericschles@gmail.com>
2014-12-31 06:39:41 -0800

    started explanation finished code

--- lesson_two.java
+++ lesson_two.java
@@ -1,3 +1,19 @@
+/*
+How to run:
+
+javac lesson_two.java
+java lesson_two
+
+Explanation:
+
+So this code is a bit more complex.  Here we've added our three function definitions: println, print, and PrintSameEveryTime.
+
+so let's understand what's happening here:
+
+A function in programming is a little different from a function in mathematics.  Unless of course you are talking about functional programming.
+First of all, functions need not return anything.  Secondly, functions need not take in any input
+
+*/
 class lesson_two{
 
 	public static void println(String input){
@@ -7,10 +23,15 @@ class lesson_two{
 	public static void print(String input){
 		println(input);
 	}
+
+	public static void PrintSameEveryTime(){
+		print("This will always print this line, and nothing else");
+	}
 	public static void main(String[] args){
 		println("Your first function");
 		println("Isn't that nice?");
 		println("");
 		print("We can even wrap our wrapped function");
+		PrintSameEveryTime();
 	}
 }
